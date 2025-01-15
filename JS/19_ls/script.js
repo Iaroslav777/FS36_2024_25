ab891bb1a8bee651ab745a388526a536b7d7886d
Iaroslav777 <wp7screencast@gmail.com>
2025-01-15 18:31:14 +0200

    add 17hw

---
+++ JS/19_ls/script.js
@@ -0,0 +1,22 @@
+
+
+class Car{
+  id = 0; 
+
+  metId(id,cb){
+console.log('Hello!',this.id);
+
+  }
+
+  constructor(){
+    this.metId();
+  }
+}
+
+let 
+
+console.log(count());
+
+let audi = new Car()
+console.log(audi);
+
